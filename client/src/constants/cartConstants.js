88d7c82e016d5dde9d93d125e573b7c656eba91b
Harry Giang <harry@rexy.tech>
2021-03-01 23:48:42 +0700

    Setup Project Redux

--- client/src/constants/cartConstants.js
+++
@@ -1,4 +0,0 @@
-export const CART_LIST_ADD = "CART_LIST_ADD";
-export const CART_LIST_DELETE = "CART_LIST_DELETE";
-export const CART_LIST_UPDATE = "CART_LIST_UPDATE";
-
--- client/src/store.js
+++ client/src/store.js
@@ -1,20 +1,11 @@
 import { applyMiddleware, combineReducers, compose, createStore }
  from "redux";
 import thunk from "redux-thunk";
-import { productListReducer } from "./reducers/productReducers";
-import { cartListReducer } from "./reducers/cartReducers";
-import { paymentCartReducer } from "./reducers/checkoutcartReducers";
-import { signupforloginReducer } from "./reducers/signupforloginReducers";
-import { myaccountReducer } from "./reducers/myaccountReducers";
-import { alertListReducer } from "./reducers/alertReducers";
+import  {Home}  from "Reducers/Home.Reducers";
 const initialState = {};
 const reducer = combineReducers({
-  productList: productListReducer,
-  cartList: cartListReducer,
-  paymentCart: paymentCartReducer,
-  signupforlogin: signupforloginReducer,
-  myaccount: myaccountReducer,
-  alertReducer: alertListReducer,
+  home: Home,
+ 
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -24,5 +15,3 @@ const store = createStore(
   composeEnhancer(applyMiddleware(thunk))
 );
 export default store;
-// Store chứa các state cũ và reduc xử lý dữ liệu
-// combineReducers chứa các phương thức xử lý các state cũ
